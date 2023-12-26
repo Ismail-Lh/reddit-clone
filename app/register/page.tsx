0a01b08c1763fac15ef69f65d3d0e41c0f49a7e0
Ismail-Lh <ismailsaad03@gmail.com>
2023-12-26 17:34:08 +0100

    feat(UI): creating the register form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 
 function RegisterPage() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleRegister = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Register page
+      <form
+        className="flex w-1/2 flex-col gap-y-4"
+        onSubmit={(e) => handleRegister(e)}
+      >
+        <input
+          className="rounded-2xl border border-gray-400 px-4 py-2"
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Enter your reddit username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          className="rounded-2xl border border-gray-400 px-4 py-2"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter your reddit password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button
+          type="submit"
+          className="rounded-full bg-[#D93A00] px-4 py-2 text-white"
+        >
+          Register
+        </button>
+      </form>
     </main>
   );
 }
